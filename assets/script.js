02115d99b2348d5f72c32849872e4887510132b1
madelineccstahl <madelinclark@outlook.com>
2023-10-28 22:04:14 -0500

    refactore code to use try, catch and async awaits, added code to fetch nutrion and sum up values, working but not correctly

--- assets/script.js
+++ assets/script.js
@@ -1,134 +1,119 @@
-//just do the calculator+organize into table
-
-//api2 should be sending back the correct information
-//(parameters per ingredient sent from api1)
-
-
-// calculate - what api2 spits out; add the different ingredients into fat, cal, etc 
-//to get a total for the meal
-
-//this information needs to be displayed in a table like a Nutrition Facts table
-
-//the fetching should already be happening w/ Nate's code
-
-//block this all out, just need it for testing
-
+// array to store strings, ingredients
 var ingredientArray = [];
-var cleanedArray = [];
 
-// base api for our MealDB to gather recipe
-var recipeApi = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+// base api for our MealDB to gather a recipe
+var recipeBaseApi = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
 
-// placeholder user entry for recipe, will eventually be replaced by user entry
+// placeholder user entry for a recipe, will eventually be replaced by user entry
 var userEntryPlaceholder = 'pizza';
 
 // concatenate MealDB API
-var recipeTestApi = recipeApi  + userEntryPlaceholder;
+var recipeTestApi = recipeBaseApi + userEntryPlaceholder;
 
 // fdc api setup, we append ingredients from MealDB api below
-var fdcNutritionApi = 'https://api.nal.usda.gov/fdc/v1/foods/search';
+var fdcNutritionBaseApi = 'https://api.nal.usda.gov/fdc/v1/foods/search?api_key=';
 var fdcApiKey = 'HdbPDR894aXY2G8mKcBsM3yf6FkjCrWrR6zYgagg';
-var ingredientPlaceholder;
-var nutritionTestApi;
 
-// first api which gathers recipe based on user entered meal
-fetch(recipeTestApi, {
-    mode: "cors",
-})
-.then(function (response) {
-    return response.json();
-})
-.then(function (data) {
-    console.log(data);
-    console.log(data.meals[0]);
-    var accessor = 'strIngredient';
+var protein_meal_sum = 0.0;
+var fat_meal_sum = 0.0;
+var carbs_meal_sum = 0.0;
+var kcal_meal_sum = 0.0;
+var sugar_meal_sum = 0.0;
+var sodium_meal_sum = 0.0;
 
-    // Loop to set up an array of ingredients which we will pass one by one to our fdc nutrition API below
-    for (var i = 1; i < 21; i++) {
-        var storer = accessor + i;
-        console.log(storer);
-        var currentIngredient = data.meals[0][storer];
 
-        ingredientArray.push(currentIngredient);
-        
-        // meant to trim the empty values and push to cleanedArray, not currently working
-        if (ingredientArray[i]) {
-            cleanedArray.push(ingredientArray[i]);
-        }
-    } 
-
-    // printing empty array for some reason
-    //console.log(cleanedArray);
-
-    // printing our full list of ingredients but has empty values which is wasteful
-    console.log(ingredientArray);
-
-    // Loop through values in ingredientArray, using each value one at a time as an argument in fdc nutrition api to retrieve 
-    // food nutrients. Currently console logging the responses
-    for (var i = 0; i < ingredientArray.length; i++) {
-        currentIngredient = ingredientArray[i];
-        nutritionTestApi = 'https://api.nal.usda.gov/fdc/v1/foods/search?api_key=' + fdcApiKey + '&query=' + currentIngredient;
-
-        fetch(nutritionTestApi, {
+// async function to fetch recipe data
+async function fetchRecipeData() {
+    try {
+        const response = await fetch(recipeTestApi, {
             mode: "cors",
-        })
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-        })
-    }
-})
+        });
 
-//
+        if (response.ok) {
+            const data = await response.json();
+            // console.log(data);
+            // console.log(data.meals[0]);
+            var accessor = 'strIngredient';
 
-//let nutrient be calories (value) for initial test purposes
-let totalNutrientValue = 0;
+            // Loop to set up an array of ingredients which we will pass one by one to our fdc nutrition API below
+            for (var i = 1; i < 21; i++) {
+                var storer = accessor + i;
+                // console.log(storer);
+                var currentIngredient = data.meals[0][storer];
 
-for (var i = 0; i < ingredientArray.length; i++) {
-    currentIngredient = ingredientArray[i];
-    nutritionTestApi = 'https://api.nal.usda.gov/fdc/v1/foods/search?api_key=' + fdcApiKey + '&query=' + currentIngredient;
-    
-    fetch(nutritionTestApi, {
-        mode: "cors",
-    })
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        //assuming data is an array of objects containing nutrients
-        if (Array.isArray(data.foods)) {
-    
-            data.foods.forEach(function (food) {
-                //extract nutrient value you want
-                //this is specific to the kcal b/c integer labeled value
-                const nutrientValue = food.value;
-
-                //check if nutrient value is a valid number
-                if (!isNaN(nutrientValue)) {
-                    //add to total
-                    totalNutrientValue += parseFloat(nutrientValue);
+                if (currentIngredient !== "") {
+                    currentIngredient = currentIngredient.trim();
+                    ingredientArray.push(currentIngredient);
                 }
-                console.log('Nutrient Value for ' + currentIngredient + ':', nutrientValue);
-            });
+            }
+        } else {
+            console.log('Error recipe fetching data:', response.status, response.statusText);
         }
-
-        //continue processing of use 'totalNutrientValue' as needed
-        //console.log('Total Nutrient Value:', totalNutrientValue);
-    });
-}
-
-/// more for calculator section than narrowing in on nutrients
-
-let energyValue = null;
-
-for (let i = 0; i < ingredientArray.length; i++) {
-    if (currentIngredient[i] === "Energy") {
-        energyValue = currentIngredient[i].value;
-        break; //exit loop, got what you need
+    } catch (error) {
+        console.error('Error fetching recipe data:', error);
     }
 }
 
-//now energyValue contains the 'kcal' value
-console.log('KCal Value:', energyValue);
+async function fetchNutritionData(ingedient) {
+    try {
+        ing_nutr_api = fdcNutritionBaseApi + fdcApiKey + '&query=' + ingedient;
+        // console.log(ing_nutr_api)
+        const response = await fetch(ing_nutr_api, {
+            mode: "cors",
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+
+            console.log("food is : ", data.foods[0]);
+
+            my_nutrients = data.foods[0]["foodNutrients"];
+
+            const findNutrientValue = (nutrientName) => {
+                const nutrient = my_nutrients.find(nutrient => nutrient.nutrientName === nutrientName);
+                return nutrient ? parseFloat(nutrient.value) : null;
+            };
+
+            let protein_grams = findNutrientValue('Protien');
+            let fat_grams = findNutrientValue('Total lipid (fat)');
+            let carbs_grams = findNutrientValue('Carbohydrate, by difference');
+            let kilo_calories = findNutrientValue('Energy');
+            let sugars = findNutrientValue('Sugars, total including NLEA');
+            let sodium = findNutrientValue('Sodium, Na');
+
+            protein_meal_sum += protein_grams;
+            fat_meal_sum += fat_grams;
+            carbs_meal_sum += carbs_grams;
+            kcal_meal_sum += kilo_calories;
+            sugar_meal_sum += sugars;
+            sodium_meal_sum += sodium;
+
+            // console.log(my_nutrients);
+
+        } else {
+            console.log('Error fetching nutrition data:', response.status, response.statusText);
+        }
+    } catch(error) {
+        console.error('Error fetching nutrition data:', error);
+    }
+}
+
+async function main() {
+    // Call fetchRecipeData with await to ensure it completes before moving on
+    await fetchRecipeData();
+
+    // Loop through each ingredient, one fetch per
+    for (i = 0; i < ingredientArray.length; i++) {
+        await fetchNutritionData(ingredientArray[i]);
+    }
+
+    console.log("Protien in meal: ", protein_meal_sum);
+    console.log("Fat in meal: ", fat_meal_sum);
+    console.log("Carbs in meal: ", carbs_meal_sum);
+    console.log("Energy in meal: ", kcal_meal_sum);
+    console.log("Sugar in meal: ", sugar_meal_sum);
+    console.log("Salt in meal: ", sodium_meal_sum);
+
+}
+
+main(); // Call the main function to start the process
