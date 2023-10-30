6c2189f8e0bfb5187b900bc9d253f99545455afc
madelineccstahl <madelinclark@outlook.com>
2023-10-29 19:08:34 -0500

    js refactor, create calculator for nutrient totals

--- assets/script.js
+++ assets/script.js
@@ -69,26 +69,29 @@ async function fetchNutritionData(ingedient) {
 
             my_nutrients = data.foods[0]["foodNutrients"];
 
-            const findNutrientValue = (nutrientName) => {
-                const nutrient = my_nutrients.find(nutrient => nutrient.nutrientName === nutrientName);
+            const findNutrientValue = (nutrientId) => {
+                const nutrient = my_nutrients.find(nutrient => nutrient.nutrientId === nutrientId);
                 return nutrient ? parseFloat(nutrient.value) : null;
+
             };
 
-            let protein_grams = findNutrientValue('Protien');
-            let fat_grams = findNutrientValue('Total lipid (fat)');
-            let carbs_grams = findNutrientValue('Carbohydrate, by difference');
-            let kilo_calories = findNutrientValue('Energy');
-            let sugars = findNutrientValue('Sugars, total including NLEA');
-            let sodium = findNutrientValue('Sodium, Na');
+            //get the numbers for each nutrient
+            let protein_grams = findNutrientValue(1003);
+            //console.log('Protein value:', protein_grams); // working w/ nutrientId!
+            let fat_grams = findNutrientValue(1004);
+            let carbs_grams = findNutrientValue(1005);
+            let kilo_calories = findNutrientValue(1008);
+            //console.log('Energy value:', kilo_calories);
+            let sugars = findNutrientValue(2000);
+            let sodium = findNutrientValue(1093);
 
+            //add up for total of each nutrient from all the ingredients
             protein_meal_sum += protein_grams;
             fat_meal_sum += fat_grams;
             carbs_meal_sum += carbs_grams;
             kcal_meal_sum += kilo_calories;
             sugar_meal_sum += sugars;
             sodium_meal_sum += sodium;
-
-            // console.log(my_nutrients);
 
         } else {
             console.log('Error fetching nutrition data:', response.status, response.statusText);
@@ -107,6 +110,8 @@ async function main() {
         await fetchNutritionData(ingredientArray[i]);
     }
 
+    //console logging total nutrients in the meal
+    //mostly working 10.29.23, maybe salt needs tweaking? kinda high for pizza
     console.log("Protien in meal: ", protein_meal_sum);
     console.log("Fat in meal: ", fat_meal_sum);
     console.log("Carbs in meal: ", carbs_meal_sum);
